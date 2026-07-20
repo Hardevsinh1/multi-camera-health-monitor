6e26c1dadaab71b78d3db85f46502a81e7dda169
Hardevsinh1 <jadejaharadevsinh3111@gmail.com>
2026-07-21 00:14:09 +0530

    Implement camera manager

--- camera-simulator/src/index.js
+++ camera-simulator/src/index.js
@@ -1,14 +1,27 @@
 import config from "./config/config.js";
 import Camera from "./camera/Camera.js";
+import CameraManager from "./manager/CameraManager.js";
 
-
+// testing simulator config (.env vars)
 console.log("Simulator Configuration");
 console.log(config);
 
-const camera = new Camera({
-    cameraId: "CAM001",
-    name: "Entrance Camera",
-    location: "Main Gate"
-});
+// camera object creation
+// const camera = new Camera({
+//     cameraId: "CAM001",
+//     name: "Entrance Camera",
+//     location: "Main Gate"
+// });
 
-console.log(camera);
+// console.log(camera);
+
+//camera manager testing 
+const manager = new CameraManager();
+
+manager.initializeCameras();
+
+manager.getAllCameras().forEach(camera => {
+    console.log(
+        `${camera.cameraId} | ${camera.name} | ${camera.location}`
+    );
+});
---
+++ camera-simulator/src/manager/CameraManager.js
@@ -0,0 +1,39 @@
+import Camera from "../camera/Camera.js";
+import config from "../config/config.js";
+
+class CameraManager {
+    constructor() {
+        this.cameras = [];
+    }
+
+    initializeCameras() {
+        const locations = [
+            "Main Gate",
+            "Parking",
+            "Warehouse",
+            "Reception",
+            "Office Floor",
+            "Server Room",
+            "Lobby",
+            "Loading Dock",
+            "Conference Room",
+            "Cafeteria",
+        ];
+
+        for (let i = 0; i < config.cameraCount; i++) {
+            const camera = new Camera({
+                cameraId: `CAM${String(i + 1).padStart(3, "0")}`,
+                name: `Camera ${i + 1}`,
+                location: locations[i % locations.length],
+            });
+
+            this.cameras.push(camera);
+        }
+    }
+
+    getAllCameras() {
+        return this.cameras;
+    }
+}
+
+export default CameraManager;
