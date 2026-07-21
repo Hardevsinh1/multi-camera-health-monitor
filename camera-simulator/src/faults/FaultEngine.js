8b957dfa2e514087487c381b5f5ad7372a733d64
Hardevsinh1 <jadejaharadevsinh3111@gmail.com>
2026-07-22 00:37:04 +0530

    Refactor fault engine using configurable fault definitions

--- camera-simulator/src/faults/FaultEngine.js
+++ camera-simulator/src/faults/FaultEngine.js
@@ -1,53 +1,67 @@
 import config from "../config/config.js";
-import faultDefinitions from "./faultDefinitions.js";
+
+import {
+    getRandomFault,
+    getFaultDefinition
+} from "./faultDefinitions.js";
 
 class FaultEngine {
+
     constructor() {
+
         this.currentFault = null;
+
         this.remainingCycles = 0;
+
     }
 
     apply(metrics) {
 
-        // Maybe start a fault
+        // Maybe start a new fault
         if (!this.currentFault) {
-    
+
             if (Math.random() < config.faultProbability) {
-    
-                const faults = Object.keys(faultDefinitions);
-    
-                this.currentFault =
-                    faults[Math.floor(Math.random() * faults.length)];
-    
+
+                this.currentFault = getRandomFault();
+
                 this.remainingCycles =
                     Math.floor(Math.random() * 6) + 5;
             }
-    
+
         }
-    
-        // No fault selected
+
+        // No fault active
         if (!this.currentFault) {
+
             return metrics;
+
         }
-    
+
         const definition =
-            faultDefinitions[this.currentFault];
-    
+            getFaultDefinition(this.currentFault);
+
         definition.apply(metrics);
-    
+
         this.remainingCycles--;
-    
+
         if (this.remainingCycles <= 0) {
-    
+
             definition.recover(metrics);
-    
+
             this.currentFault = null;
-    
+
         }
-    
+
         return metrics;
-    
+
     }
+
+    getCurrentFault() {
+
+        return this.currentFault;
+
+    }
+
 }
 
 export default FaultEngine;
--- camera-simulator/src/faults/faultDefinitions.js
+++ camera-simulator/src/faults/faultDefinitions.js
@@ -5,7 +5,15 @@ const randomBetween = (min, max) =>
 
 const faultDefinitions = {
 
+
     HIGH_CPU: {
+
+        probability: 0.03,
+
+        duration: {
+            min: 5,
+            max: 10
+        },
 
         apply(metrics) {
 
@@ -27,24 +35,87 @@ const faultDefinitions = {
     HIGH_MEMORY: {
 
         apply(metrics) {
-    
+
             metrics.memory = randomBetween(92, 98);
-    
+
         },
-    
+
         recover(metrics) {
-    
+
             metrics.memory = randomBetween(
                 config.healthyRanges.memory.min,
                 config.healthyRanges.memory.max
             );
-    
+
         }
-    
+
     },
 
-    
+    HIGH_STORAGE: {
+        apply(metrics) {
+
+            metrics.storageUsed =
+                metrics.storageCapacity * 0.98;
+
+        },
+
+        recover(metrics) {
+
+            metrics.storageUsed =
+                randomBetween(50, 55);
+
+        }
+    },
+
+    HIGH_LATENCY: {
+        apply(metrics) {
+
+            metrics.latency = randomBetween(300, 500);
+
+        },
+
+        recover(metrics) {
+
+            metrics.latency = randomBetween(
+                config.healthyRanges.latency.min,
+                config.healthyRanges.latency.max
+            );
+
+        }
+    },
+
+    OFFLINE: {
+        apply(metrics) {
+
+            metrics.online = false;
+
+        },
+        recover(metrics) {
+
+            metrics.online = true;
+
+        }
+    }
+
+
 
 };
+
+
+
+export const getRandomFault = () => {
+
+    const faults = Object.keys(faultDefinitions);
+
+    return faults[
+        Math.floor(Math.random() * faults.length)
+    ];
+
+}
+
+export const getFaultDefinition = (faultName) =>
+    faultDefinitions[faultName];
+
+
 
 export default faultDefinitions;
